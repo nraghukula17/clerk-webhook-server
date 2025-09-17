072f8ec05cf1bb1283e99c9df55f3e2bcf3204d6
nraghukula17 <nxr210025@gmail.com>
2025-09-17 09:41:09 -0500

    Delete app.ts

--- app.ts
+++
@@ -1,9 +0,0 @@
-import express from 'express';
-import clerkWebhookRouter from './webhooks/clerkWebhook';
-
-const app = express();
-
-// Mount the webhook router
-app.use(clerkWebhookRouter);
-
-export default app;
